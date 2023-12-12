8f9138a50adcebd6f3531fad5dd7463eb98e6dd9
ismail ait zaid <ismailaitzaid6@gmail.com>
2023-12-12 02:27:54 +0100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <?php
 
-echo "Hello, World from Ismail to all of you the test is done wooooooooow :) :) :)! <br>";
+echo "Hello, World from Ismail to all of you the test is done wooooooooow :) :) :)! Yogiiiii <br>";
 echo '<img src="src/ctd_consulting.jpg">';
 ?>
