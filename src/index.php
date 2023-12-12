964fda28d3c3e652a77172d3bf3bce8c99699f59
ismail ait zaid <ismailaitzaid6@gmail.com>
2023-12-12 02:23:44 +0100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,6 +1,5 @@
 <?php
 
 echo "Hello, World from Ismail to all of you the test is done wooooooooow :) :) :)! <br>";
-echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
-
+echo '<img src="src/ctd_consulting.jpg">';
 ?>
