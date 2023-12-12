3b631c56dcb22d63ca565aa8fef18318d911bb87
ismail ait zaid <ismailaitzaid6@gmail.com>
2023-12-12 02:15:05 +0100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,7 +1,6 @@
 <?php
 
-echo "Hello, World from Docker! <br>";
-echo "Hello ECS! <br>";
+echo "Hello, World from Ismail to all of you the test is done wooooooooow :) :) :)! <br>";
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 
 ?>
