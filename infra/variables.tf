bcd8ebde5a8c9ee851ce08cca0729859e7212c2c
ismail ait zaid <ismailaitzaid6@gmail.com>
2023-12-13 18:47:37 +0100

    Add files via upload

--- infra/variables.tf
+++ infra/variables.tf
@@ -18,6 +18,25 @@ variable ecr_image {
   default = "200268818177.dkr.ecr.eu-west-1.amazonaws.com/github-action-ecr:latest"
 }
 
+
+
+variable ecs_instance_ami_id {
+  default = "ami-049b0abf844cab8d7"
+}
+
+
+variable ecs_instance_type {
+  default = "t2.micro"
+}
+
 variable desired_count {
-  default = 2
+  default = "3"
+}
+
+variable max_count {
+  default = "4"
+}
+
+variable min_count {
+  default = "2"
 }
